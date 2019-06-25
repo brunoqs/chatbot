7f3664e557ed648d5dcd4ebf2c1f3176b573d717
williamabreu <williamsantos_00@hotmail.com>
2019-06-24 22:47:49 -0300

    Organizado código-fonte

--- chatbot.py
+++ chatbot.py
@@ -1,31 +1,47 @@
 import os
+import time
 import random
 import string
 import warnings
 import nltk
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-GREETING_INPUTS = ["ola", "oi", "opa", "eae"]
-GREETING_RESPONSES = ["ola", "oi", "opa", "eae"]
+
+# Configuração para ignorar warnings
+warnings.filterwarnings('ignore')
+
+# Saudações
+GREETING_INPUTS = ['ola', 'oi', 'opa', 'eae']
+GREETING_RESPONSES = ['ola', 'oi', 'opa', 'eae']
+
 
 class Chatbot:
-
+    """
+    Representa a entidade do chatbot
+    """
     def __init__(self):
-        nltk.download('popular', quiet=True)
-        warnings.filterwarnings('ignore')
-
-        self.sent_tokens = []
+        """
+        Instancia o objeto configurado para português.
+        """
         self.stopwords = nltk.corpus.stopwords.words('portuguese')
         self.remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
-        self.lemmer =  nltk.stem.WordNetLemmatizer()
+        self.lemmer = nltk.stem.WordNetLemmatizer()
 
+        self.sent_tokens = []
         self.sent_tokens.extend(self.load_sentences('corpora/tecnologia/'))
         self.sent_tokens.extend(self.load_sentences('corpora/mercado/'))
         self.sent_tokens.extend(self.load_sentences('corpora/telefonia/'))
         self.sent_tokens.extend(self.load_sentences('corpora/saude/'))
         
-    def load_sentences(self, corpus_dir):   
+    def load_sentences(self, corpus_dir: str) -> list:
+        """
+        Carrega as sentenças dos corpora.
+
+        :param corpus_dir: Caminho do diretório dos corpora, terminado com 
+        :return: Lista com todas as sentenças
+        """
         all_sentences = []
         for file_path in os.listdir(corpus_dir):
             with open(corpus_dir + file_path) as fp:
@@ -34,31 +50,60 @@ class Chatbot:
                     all_sentences.extend(sentences)
         return all_sentences
 
-    def lem_tokens(self, tokens):
+    def lem_tokens(self, tokens: list) -> list:
+        """
+        Lematiza os tokens.
+
+        :param tokens: Lista com tokens
+        :return: Lista com tokens lematizados
+        """
         return [self.lemmer.lemmatize(token) for token in tokens]
 
-    def lem_normalize(self, text):
+    def lem_normalize(self, text: str) -> list:
+        """
+        Normaliza o texto gerando lista de tokens.
+
+        :param text: Texto qualquer de entrada
+        :return: Lista de tokens normalizados
+        """
         return self.lem_tokens(nltk.word_tokenize(text.lower().translate(self.remove_punct_dict)))
 
-    def greeting(self, sentence):
+    def greeting(self, sentence: str) -> str:
+        """
+        Gera resposta para cumprimento.
+
+        :param sentence: Sentença de entrada
+        :return: Resposta de cumprimento se identificada saudação
+        """
         for word in sentence.split():
             if word.lower() in GREETING_INPUTS:
                 return random.choice(GREETING_RESPONSES)
+        return ''
 
-    def response(self, user_request):
-        bot_response = ''
-        self.sent_tokens.append(user_request)
-        TfidfVec = TfidfVectorizer(tokenizer=self.lem_normalize, stop_words=self.stopwords)
-        tfidf = TfidfVec.fit_transform(self.sent_tokens)
+    def response(self, user_request: str) -> str:
+        """
+        Gera resposta para pergunta feita.
+
+        :param user_request: Pergunta de entrada
+        :return: Resposta se for possível respondê-la
+        """
+        request = ' '.join(self.lem_normalize(user_request))
+        self.sent_tokens.append(request)
+        
+        tfidf_vec = TfidfVectorizer(tokenizer=self.lem_normalize, stop_words=self.stopwords)
+        tfidf = tfidf_vec.fit_transform(self.sent_tokens)
+        
         vals = cosine_similarity(tfidf[-1], tfidf)
-        idx = vals.argsort()[0][-2]
+        
+        idx = vals.argsort()[0][-2]        
         flat = vals.flatten()
         flat.sort()
+        
         req_tfidf = flat[-2]
+        
+        self.sent_tokens.pop()
 
         if(req_tfidf == 0):
-            bot_response = bot_response + "Desculpe, não consigo entender essa pergunta"
-            return bot_response
+            return 'Desculpe, não consigo entender essa pergunta.'
         else:
-            bot_response = bot_response + self.sent_tokens[idx]
-            return bot_response
+            return self.sent_tokens[idx]
--- main.py
+++ main.py
@@ -1,25 +1,39 @@
+import time
+
 from chatbot import Chatbot
 
-chatbot = Chatbot()
 
-flag = True
-print("BOT: Olá querido usuário, fico a disposição para responder perguntas sobre mercado, saúde, tecnologia e telefonia. Diga tchau quando encerrar!")
+def main():
+    chatbot = Chatbot()
 
-while(flag == True):
-    user_request = input('Pergunta: ')
-    user_request = user_request.lower()
+    print('BOT: Olá querido usuário!')
+    print('BOT: Fico a disposição para responder perguntas sobre mercado, saúde, tecnologia e telefonia.')
+    print('BOT: Diga tchau quando encerrar!')
+    
+    stop = False
 
-    if(user_request != 'tchau'):
-        if(user_request == 'obrigado' or user_request == 'muito obrigado'):
-            flag = False
-            print("BOT: Não há de que...")
-        else:
-            if(chatbot.greeting(user_request) != None):
-                print("BOT: " + chatbot.greeting(user_request))
+    while not stop:
+        user_request = input('Pergunta: ')
+        user_request = user_request.lower()
+
+        if user_request != 'tchau':
+            if user_request == 'obrigado' or user_request == 'muito obrigado':
+                stop = True
+                print("BOT: Não há de quê!")
             else:
-                print("BOT: Pensando...")
-                print("BOT: " + chatbot.response(user_request))
-                chatbot.sent_tokens.remove(user_request)
-    else:
-        flag = False
-        print("BOT: Até mais...")    
+                if chatbot.greeting(user_request) != '':
+                    print("BOT:", chatbot.greeting(user_request))
+                else:
+                    print("BOT: Pensando...")
+                    start_time = time.perf_counter()
+                    response = chatbot.response(user_request)
+                    end_time = time.perf_counter()
+                    print("BOT:", response)
+                    print('[{:.1f} segundos]'.format(end_time - start_time))
+        else:
+            stop = True
+            print("BOT: Até mais!")
+
+
+if __name__ == '__main__':
+    main()
